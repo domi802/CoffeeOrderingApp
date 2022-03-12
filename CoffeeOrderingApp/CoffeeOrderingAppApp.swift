9ef349552d48fbf3d89452f7f04eaf0f12215e75
domi802 <64065747+domi802@users.noreply.github.com>
2022-03-12 14:11:54 +0100

    Initial Commit

---
+++ CoffeeOrderingApp/CoffeeOrderingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeOrderingAppApp.swift
+//  CoffeeOrderingApp
+//
+//  Created by Dominik Woźniak on 12/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeOrderingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
